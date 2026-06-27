ed0f14b5a65b60faee6ced1c42860f5de942a4bc
yolorys <yelarysseidin05@gmail.com>
2026-06-27 11:46:54 -0500

    Add wget command for April 2026 data

--- download_data.sh
+++ download_data.sh
@@ -9,4 +9,7 @@ wget -nc -P ./data "https://huggingface.co/datasets/thomasd1/aix-lichess-databas
 # Download March 2026 data
 wget -nc -P ./data "https://huggingface.co/datasets/thomasd1/aix-lichess-database/resolve/main/low_compression/aix_lichess_2026-03_low.parquet"
 
+# Download April 2026 data
+wget -nc -P ./data "https://huggingface.co/datasets/thomasd1/aix-lichess-database/resolve/main/low_compression/aix_lichess_2026-04_low.parquet"
+
 echo "Downloads complete."
